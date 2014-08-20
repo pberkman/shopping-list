921797ad487047224a6c1e0c38d5929ac47346ee
pberkman <patriciaberkman@Patricias-MacBook-Pro.local>
2014-08-20 18:21:35 -0400

    html dom edits

--- js/app.js
+++ js/app.js
@@ -3,45 +3,46 @@ $(document).ready(function() {
 	
 	$("#addButton").on("click", function(){
 		var added = $("#newItem").val();
-		$("ul").append('<li <span class="indItem">' + added + '</span><span class="remove"></span><span class="edit"></span></li>');			
+		$("ul").append('<li <span class="indItem">' + added + '</span><span class="remove"></span><span class="edit"></span></li>');
 		$("#newItem").val("");
-		})
+	});
 
 	/*using then enter key to add item to the list*/
 	$("#newItem").keypress(function(event){
 		if (event.which == 13 ){
-    	  event.preventDefault();
-    	  event.defaultPrevented();
-    	  var added = $("#newItem").val();
-		  $('<li <span class="indItem">' + added + '</span><span class="remove"></span><span class="edit"></span></li>').appendTo("ul"); 
-		  $("#newItem").val("");
+			event.preventDefault();
+			var added = $("#newItem").val();
+			$('<li <span class="indItem">' + added + '</span><span class="remove"></span><span class="edit"></span></li>').appendTo("ul");
+			$("#newItem").val("");
 		}
-	})
+	});
 	/*clearing input value on focus*/
 	$("#newItem").on("click",function(){
 		$(this).removeAttr("placeholder");
-	})
+	});
 	
 	/*making the edit and remove buttons appear */
 	$("ul").on("mouseenter", "li",function() {
-	  $(this).find(".remove").show();
-	  $(this).find(".edit").show();
-	})
+		$(this).find(".remove").show();
+		$(this).find(".edit").show();
+	});
 	$("ul").on("mouseleave", "li",function() {
 		$(this).find(".remove").hide();
 		$(this).find(".edit").hide();
-	})
+	});
+
 	/*select the remove button to take item off list*/
 	$("ul").on("click", ".remove", function(){
 		var remove=$(this).closest(".indItem");
 		$(remove).remove();
-	})
+	});
+
 	/*select the edit button to edit the item*/
 	$(".edit").on("click", function(){
 		var edit=$(this).closest("indItem").focus();
-	})
+	});
 	/*click on item to check off*/
 	$("ul").on("click", ".indItem",function(){
 		$(this).closest(".indItem").toggleClass("crossedOff");
-	})
+	});
 });
